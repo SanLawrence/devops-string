964f1721fc6f7f709d91148f9e2f672fe04ec075
Saint Lawrence <saintlawrencedias@gmail.com>
2023-11-12 17:32:00 -0400

    adds unit test for string standardization function

--- src/stringstd.py
+++
@@ -1,8 +0,0 @@
-from unidecode import unidecode
-
-def standardize(text):
-    return unidecode(text)
-
-nome = "Saint Lawrence Sevalho Gonçalves Dias"
-std_nome = standardize(nome)
-print(f"Seu nome é: {nome}\nSeu nome sem acento é: {std_nome}")
---
+++ test/test_stringstd.py
@@ -0,0 +1,8 @@
+from stringstandardization import stringstd
+
+def test_full_name():
+    name = "Saint Gonçalves"
+    std_name = stringstd.standardize(name)
+    assert len(std_name.split(" ")) > 1
+
+     
